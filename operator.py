7192a6093054f8e002a53e9e3851d1a01ed49516
phamxten <phamxten@gmail.com>
2026-05-04 20:55:55 +0700

    Add files via upload

--- operator.py
+++ operator.py
@@ -5,11 +5,70 @@ print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
+
+print(a // b) #hasil nya karena di bulatkan 3x3 = 9 sisa 1 jadi ya 3
 print(a % b)
-print(15 * b)
-print(17 % 2)
+
+print(10 % 2)
+print(11 % 2)
 
 a = 2
 b = 3
+
 print(a ** b)
 print(a // b)
+
+x = 10
+print(x)
+x += 5
+print(x)
+x -= 2
+print(x)
+x *= 4
+print(x)
+x /= 2
+print(x)
+
+a = 5
+b = 4
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+print("==================================")
+
+nama1 = "Vann"
+nama2 = "px"
+nama3 = "Vann"
+print(nama1 == nama3)
+print(nama1 != nama2)
+print(nama1 != nama3)
+
+print("==================================")
+umur = 19
+print(umur > 17 and umur < 23)
+
+hari = "jumat"
+print(hari == "sabtu" or hari == "jumat")
+
+active = True
+print(not active)
+
+nama_depan = "Yohan" 
+nama_belakang = "Gangbuk"
+nama_lengkap = nama_depan + " " + nama_belakang
+print(nama_lengkap)
+
+say = "hello"
+print(say * 3)
+
+garis = "="
+print(garis * 30)
+
+kalimat = "python adalah bahasa  pemograman tingkat rendah"
+print("python" in kalimat)
+print("java" in kalimat)
+print("python" in kalimat)
