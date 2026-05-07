17a3a2cfdf5d01687eab8417ed227daba904e0a1
phamxten <phamxten@gmail.com>
2026-05-07 07:03:03 +0700

    Add files via upload

--- if.py
+++ if.py
@@ -8,9 +8,30 @@
 #     print("angka nol")
 
 
-nilai = int(input("Masukkan Nilai :"))
+# nilai = int(input("Masukkan Nilai :"))
 
-if nilai >= 70:
-    print("Lulus Ujian")
+# if nilai >= 70:
+#     print("Lulus Ujian")
+# else:
+#     print("Remed")
+
+# nilai = int(input("Masukkan Nilai Siswa :"))
+
+#   if nilai >= 90:
+#      print("Grade A")
+#   elif nilai >= 80:
+#      print("Grade B")
+#   elif nilai >= 70:
+#     print("Grade C")
+#   else:
+#      print("Grade E")
+
+umur = int(input("Masukkan Umur Anda :"))
+buat_SIM = input("Apakah Anda Sudah memiliki SIM? (ya/belum) ")
+
+if umur >= 17 and buat_SIM == "ya":
+    print("Anda Boleh Mengendarai Kendaraan")
 else:
-    print("Remed")
+    print("Anda Belum Boleh Mengendarai Kendaaraan")
+
+
